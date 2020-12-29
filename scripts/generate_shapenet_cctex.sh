2921a78eb034be14d1e91ec89331cd3d4a1a852d
Qiao Gu <georgegu1997@gmail.com>
2020-12-29 17:06:27 -0500

    adjust the setting of the rendering pipeline

--- scripts/generate_shapenet_cctex.sh
+++ scripts/generate_shapenet_cctex.sh
@@ -7,7 +7,7 @@ do
     echo "$i"
     CUDA_VISIBLE_DEVICES=0 python run.py \
     examples/shapenet_with_cctex/config.yaml \
-    ~/datasets/render/shapenetcc/ \
+    ~/datasets/render/shapenetcc2/ \
     ~/datasets/shapenet/ShapeNetCore.v2/ \
     /home/qiaog/datasets/cctextures_processed/ \
     ~/datasets/render/shapenetcc/ \
